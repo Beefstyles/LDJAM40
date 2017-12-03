9fd5ce43bb75a133f85e8d14934a0c041fec4089
Glen Willis <glenwillis@gmail.com>
2017-12-03 20:48:08 +0800

    Added in the timing gates.

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -11,13 +11,49 @@ public class GameController : MonoBehaviour {
     private GameObject playerClone;
     LapTimer lapTimer;
 
+    int numberOfGatesTrigged = 0;
+    int numberOfGates = 0;
+    public bool CanFinishLap = false;
+
+    TimingGate[] timingGates;
+
     public void StartLap()
     {
+        numberOfGates = 0;
         lapTimer.ResetTimer();
         if(playerClone != null)
         {
             Destroy(playerClone);
         }
         playerClone = Instantiate(PlayerCar, PlayerSpawnLocation.position, Quaternion.identity) as GameObject;
+
+        timingGates = FindObjectsOfType<TimingGate>();
+        foreach (var gate in timingGates)
+        {
+            numberOfGates++;
+            gate.TimingGateTriggered = false;
+        }
+    }
+
+    void Start()
+    {
+        
+    }
+
+    public void CheckIfAllGatesHit()
+    {
+        timingGates = FindObjectsOfType<TimingGate>();
+        foreach (var gate in timingGates)
+        {
+            if (gate.TimingGateTriggered)
+            {
+                numberOfGatesTrigged++;
+            }
+        }
+
+        if(numberOfGatesTrigged >= numberOfGates)
+        {
+
+        }
     }
 }
--- Assets/Scripts/TimingGate.cs
+++ Assets/Scripts/TimingGate.cs
@@ -1,15 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TimingGate : MonoBehaviour {
 
+    public bool TimingGateTriggered;
+    GameController gc;
+
+    void Start()
+    {
+        gc = FindObjectOfType<GameController>();
+    }
 
     void OnTriggerEnter2D(Collider2D coll)
     {
         if (coll.tag == "PlayerCar")
         {
-
+            TimingGateTriggered = true;
+            gc.CheckIfAllGatesHit();
         }
     }
 }
