d807f2ba12af944e5c151d2629d143f534bad1d6
Glen Willis <glenwillis@gmail.com>
2017-12-03 09:10:52 +0800

    Driftu

--- Assets/Scripts/CarController.cs
+++ Assets/Scripts/CarController.cs
@@ -6,6 +6,7 @@ public class CarController : MonoBehaviour {
 
     float carSpeed = 10F;
     float torqueForce = -2F;
+    float driftFactor = 0.9F;
     Rigidbody2D rb;
 
 
@@ -23,10 +24,18 @@ public class CarController : MonoBehaviour {
         }
 
         rb.AddTorque(Input.GetAxis("Horizontal") * torqueForce);
+
+        rb.velocity = ForwardVelocity() + RightVelocity()*driftFactor;
     }
 
-	void Update ()
+    Vector2 ForwardVelocity()
     {
+        return transform.up * Vector2.Dot(rb.velocity, transform.up);
+    }
 
-	}
+    Vector2 RightVelocity()
+    {
+        return transform.right * Vector2.Dot(rb.velocity, transform.right);
+    }
+
 }
