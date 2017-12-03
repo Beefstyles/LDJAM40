730152d9fff4e642fe24038f2da3b6b749d1bcf3
Glen Willis <glenwillis@gmail.com>
2017-12-03 11:45:45 +0800

    Added more of the game parts.

---
+++ Assets/Scripts/StartFinishGate.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartFinishGate : MonoBehaviour {
+
+
+    LapController lapController;
+
+	void Start ()
+    {
+        lapController = FindObjectOfType<LapController>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerExit2D(Collider2D coll)
+    {
+        if(coll.tag == "PlayerCar")
+        {
+            if (!lapController.LapStarted)
+            {
+                lapController.LapStarted = true;
+            }
+            Debug.Log("Exited Through Gate");
+        }
+    }
+
+
+}
