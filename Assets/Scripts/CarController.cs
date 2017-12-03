683c7def6d9eca20ad1802f7f4102378c479267f
Glen Willis <glenwillis@gmail.com>
2017-12-03 08:56:06 +0800

    Bit of a sprint.

---
+++ Assets/Scripts/CarController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarController : MonoBehaviour {
+
+    float carSpeed = 10F;
+    float torqueForce = 10F;
+    Rigidbody2D rb;
+
+
+    void Start ()
+    {
+        rb = GetComponent<Rigidbody2D>();
+	}
+	
+
+    void FixedUpdate()
+    {
+        if (Input.GetButton("Accelerate"))
+        {
+            rb.AddForce(transform.up * carSpeed);
+        }
+
+        rb.AddTorque(Input.GetAxis("Horizontal") * torqueForce);
+    }
+
+	void Update ()
+    {
+
+	}
+}
